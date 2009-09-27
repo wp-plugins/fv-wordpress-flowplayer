d045398258062085250b002e4a41dc9c1d20a779
FolioVision <FolioVision@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-27 10:09:14 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/fv-wordpress-flowplayer/trunk@158356 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ flowplayer.php
@@ -0,0 +1,25 @@
+<?PHP
+/*
+Plugin Name: FV Wordpress Flowplayer
+Plugin URI: http://foliovision.com/seo-tools/wordpress/plugins/fv-wordpress-flowplayer
+Description: Embed videos (FLV, H.264, and MP4) into posts or pages. Uses modified version of flowplayer (with removed FP logo and copyright notice). 
+Version: 0.9.12.
+Author: Foliovision
+Author URI: http://foliovision.com/
+*/
+
+
+if(is_admin()) {
+	/**
+	 * If administrator is logged, loads the controller for backend.
+	 */
+	include( dirname( __FILE__ ) . '/controller/backend.php' );
+} else {
+	/**
+	 * If administrator is not logged, loads the controller for frontend.
+	 */
+	include( dirname( __FILE__ ) . '/controller/frontend.php' );
+}
+
+
+?>
