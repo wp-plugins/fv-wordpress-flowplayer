86e51255d3b16fb286db9ed1a82f14ecf86ddfb6
FolioVision <FolioVision@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-09 14:09:22 +0000

    git-svn-id: https://plugins.svn.wordpress.org/fv-wordpress-flowplayer/tags/0.9.13@161869 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ view/backend-head.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Displays metatags for administrator backend.
+ */
+?>
+<!-- FV Flowplayer For Wordpress ADMIN Javascript Start -->
+<script type="text/javascript" src="<?php echo RELATIVE_PATH; ?>/js/jscolor/jscolor.js"></script>
+<script type="text/javascript" src="<?php echo RELATIVE_PATH; ?>/flowplayer/flowplayer.min.js"></script>
+<link rel="stylesheet" href="<?php echo RELATIVE_PATH; ?>/css/flowplayer.css" type="text/css" media="screen" />
+<!-- FV Flowplayer For Wordpress ADMIN Javascript END -->
+
