fd66fcdeabefc1123750af4cda8f247c7d1997c1
FolioVision <FolioVision@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-13 15:26:23 +0000

    adding prev version 1.0.6
    git-svn-id: https://plugins.svn.wordpress.org/fv-wordpress-flowplayer/tags/1.0.6@275437 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ controller/shortcodes.php
@@ -0,0 +1,85 @@
+<?php
+require_once dirname( __FILE__ ) . '/../models/flowplayer.php';
+//require_once dirname( __FILE__ ) . '/../models/flowplayer-frontend.php';
+
+
+add_shortcode('flowplayer','flowplayer_content_handle');
+
+function flowplayer_content_handle( $atts ) {
+  /// Addition  2010/07/12  mv 
+  $fp = new flowplayer_frontend();
+  if( $fp->conf['commas'] == 'true' ) {
+    
+    if( !isset( $atts['src'] ) ) {
+      foreach( $atts AS $key => $att ) {
+        if( stripos( $att, 'src=' ) !== FALSE ) {
+          $atts['src'] = preg_replace( '/^\s*?src=[\'"](.*)[\'"],\s*?$/', '$1', $att );
+          $i = $key+1;
+          unset( $atts[$key] ); // = ''; //  let's remove it, so it won't confuse the rest of workaaround
+        }
+      }
+    }
+  
+    if( !isset( $atts['splash'] ) ) {
+      foreach( $atts AS $key => $att ) {
+        if( stripos( $att, 'splash=' ) !== FALSE ) {
+          $atts['splash'] = preg_replace( '/^\s*?splash=[\'"](.*)[\'"],\s*?$/', '$1', $att );
+          unset( $atts[$key] ); // = ''; //  let's remove it, so it won't confuse the rest of workaround
+        }
+      }
+    }
+  
+    //  the popup should really be a content of the shortcode, not an attribute
+    //  this part will fix the popup if there is any single quote in it.
+    if( !isset( $atts['popup'] ) ) {
+      $popup = array();
+      $is_popup = false;
+      foreach( $atts AS $key => $att ) {
+        if( !is_numeric( $key ) ) continue;
+        if( ( stripos( $att, 'popup=' ) !== FALSE || $is_popup ) && stripos( $att, 'src=' ) === FALSE && stripos( $att, 'splash=' ) === FALSE) {
+          $popup[] = $att;
+          $is_popup = true;
+          unset( $atts[$key] ); // = ''; //  let's remove it, so it won't confuse the rest of workaround
+        }
+      }
+      $popup = implode( ' ', $popup );
+      $atts['popup'] = preg_replace( '/^\s*?popup=[\'"](.*)[\'"]\s*?$/mi', '$1', $popup );
+    }
+    
+  }
+  /// End of addition
+  
+  extract( shortcode_atts( array(
+      'src' => '',
+      'width' => '',
+      'height' => '',
+      'autoplay' => '',
+      'splash' => '',
+      'popup' => '',
+      'controlbar' => '',
+      'redirect' => ''
+      ), $atts ) );
+  
+	$arguments['width'] = preg_replace('/\,/', '', $width);
+	$arguments['height'] = preg_replace('/\,/', '', $height);
+	$arguments['autoplay'] = preg_replace('/\,/', '', $autoplay);
+//	$arguments['embed'] = $embed;
+	$arguments['splash'] = preg_replace('/\,/', '', $splash);
+	$arguments['popup'] = $popup;
+	$arguments['controlbar'] = preg_replace('/\,/', '', $controlbar);
+	$arguments['redirect'] = preg_replace('/\,/', '', $redirect);
+
+	$src = preg_replace('/\,/', '', $src);
+	if (trim($src) != '') {
+		// build new player
+	  //$fp = new flowplayer_frontend();
+    $new_player = $fp->build_min_player($src,$arguments);
+    //  var_dump($new_player['html']); 
+		$content = str_replace($src, $new_player['html'],$atts);
+		$GLOBALS['scripts'][] = $new_player['script'];
+    // var_dump($content); 
+	}
+    return $new_player['html'];
+//	return $content;
+}
+?>
---
+++ models/flowplayer.php
@@ -0,0 +1,117 @@
+<?php
+ 
+class flowplayer {
+	private $count = 0;
+	
+	/**
+	 * Relative URL path
+	 */
+	const RELATIVE_PATH = '';
+	/**
+	 * Where videos should be stored
+	 */
+	const VIDEO_PATH = '';
+	/**
+	 * Where the config file should be
+	 */
+	private $conf_path = '';
+	
+	/**
+	 * Configuration variables array
+	 */
+	public $conf = array();
+	
+	/**
+	 * Class constructor
+	 */
+	public function __construct() {
+		//set conf path
+		///$this->conf_path = realpath(dirname(__FILE__)).'/../wpfp.conf';
+		//load conf data into stack
+		$this->_get_conf();
+	}
+	/**
+	 * Gets configuration from cfg file.
+	 * 
+	 * @return bool Returns false on failiure, true on success.
+	 */
+	private function _get_conf() {
+	  ///  Addition  2010/07/12  mv
+    $conf = get_option( 'fvwpflowplayer' );
+    
+    if( !isset( $conf['autoplay'] ) ) $conf['autoplay'] = 'false';
+    if( !isset( $conf['key'] ) ) $conf['key'] = 'false';
+    if( !isset( $conf['autobuffer'] ) ) $conf['autobuffer'] = 'false';
+    if( !isset( $conf['popupbox'] ) ) $conf['popupbox'] = 'false';
+    if( !isset( $conf['allowfullscreen'] ) ) $conf['allowfullscreen'] = 'true';
+    if( !isset( $conf['allowuploads'] ) ) $conf['allowuploads'] = 'true';
+    if( !isset( $conf['postthumbnail'] ) ) $conf['postthumbnail'] = 'false';
+    if( !isset( $conf['tgt'] ) ) $conf['tgt'] = 'backgroundcolor';
+    if( !isset( $conf['backgroundColor'] ) ) $conf['backgroundColor'] = '#1b1b1d';
+    if( !isset( $conf['canvas'] ) ) $conf['canvas'] = '#ffffff';
+    if( !isset( $conf['sliderColor'] ) ) $conf['sliderColor'] = '#2e2e2e';
+    if( !isset( $conf['buttonColor'] ) ) $conf['buttonColor'] = '#454545';
+    if( !isset( $conf['buttonOverColor'] ) ) $conf['buttonOverColor'] = '#ffffff';
+    if( !isset( $conf['durationColor'] ) ) $conf['durationColor'] = '#ffffff';
+    if( !isset( $conf['timeColor'] ) ) $conf['timeColor'] = '#ededed';
+    if( !isset( $conf['progressColor'] ) ) $conf['progressColor'] = '#707070';
+    if( !isset( $conf['bufferColor'] ) ) $conf['bufferColor'] = '#4d4d4d';
+    if( !isset( $conf['commas'] ) ) $conf['commas'] = 'true';
+    
+    update_option( 'fvwpflowplayer', $conf );
+    $this->conf = $conf;
+    return true;	 
+    /// End of addition
+	}
+	/**
+	 * Writes configuration into file.
+	 */
+	public function _set_conf() {
+	  //var_dump( $_POST );
+	  foreach( $_POST AS $key => $value ) {
+	    if( (strpos( $key, 'Color' ) !== FALSE )||(strpos( $key, 'canvas' ) !== FALSE)) {
+	      $_POST[$key] = '#'.strtolower($value);
+	    }
+	  }
+	  update_option( 'fvwpflowplayer', $_POST );
+	  return;
+	  
+	}
+	/**
+	 * Salt function - returns pseudorandom string hash.
+	 * @return Pseudorandom string hash.
+	 */
+	public function _salt() {
+        $salt = substr(md5(uniqid(rand(), true)), 0, 10);    
+        return $salt;
+	}
+
+	
+}
+
+/**
+ * Defines some needed constants and loads the right flowplayer_head() function.
+ */
+function flowplayer_head() {
+	// define needed constants
+   preg_match('/.*wp-content\/plugins\/(.*?)\/models.*/',dirname(__FILE__),$matches);
+   if (isset($matches[1]))
+      $strFPdirname = $matches[1];
+   else
+       $strFPdirname = 'fv-wordpress-flowplayer';
+	if (!defined('RELATIVE_PATH')) {
+		define('RELATIVE_PATH', get_option('siteurl').'/wp-content/plugins/'.$strFPdirname);
+		define('PLAYER', RELATIVE_PATH.'/flowplayer/flowplayer.swf');
+		$vid = 'http://'.$_SERVER['SERVER_NAME'];
+		if (dirname($_SERVER['PHP_SELF']) != '/') $vid .= dirname($_SERVER['PHP_SELF']);
+		define('VIDEO_PATH', $vid.'/videos/');
+	}
+	// call the right function for displaying CSS and JS links
+	if (class_exists('flowplayer_frontend')) {
+		flowplayer_frontend::flowplayer_head();
+	} else {
+		flowplayer_backend::flowplayer_head();
+	}
+}
+
+?>
