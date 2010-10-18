e473ce433f9108c45963b3c2cc95732ac359a516
FolioVision <FolioVision@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-18 19:30:07 +0000

    typo
    git-svn-id: https://plugins.svn.wordpress.org/fv-wordpress-flowplayer/trunk@301322 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- models/flowplayer.php
+++ models/flowplayer.php
@@ -106,7 +106,7 @@ function flowplayer_head() {
 		if( !isset( $conf['key'] )||(!$conf['key'])||($conf['key']=='false') )
       define('PLAYER', RELATIVE_PATH.'/flowplayer/flowplayer.swf');
     else
-      define('PLAYER', RELATIVE_PATH.'/flowplayer/commercial/flowplayer.swf');
+      define('PLAYER', RELATIVE_PATH.'/flowplayer/commercial/flowplayer.commercial-3.1.5.swf');
     
 		$vid = 'http://'.$_SERVER['SERVER_NAME'];
 		if (dirname($_SERVER['PHP_SELF']) != '/') $vid .= dirname($_SERVER['PHP_SELF']);
