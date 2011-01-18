fd1b9be0f3b9445446baa92d4d6c0bd4929ea780
FolioVision <FolioVision@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-18 09:36:09 +0000

    git-svn-id: https://plugins.svn.wordpress.org/fv-wordpress-flowplayer/trunk@334004 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- view/frontend-head.php
+++ view/frontend-head.php
@@ -18,8 +18,8 @@
 <script type="text/javascript" src="<?php echo RELATIVE_PATH; ?>/flowplayer/flowplayer.min.js"></script>
 <?php }  ?>
 <link rel="stylesheet" href="<?php echo RELATIVE_PATH; ?>/css/flowplayer.css" type="text/css" media="screen" />
-<!--[if lt IE 7.]>
 <?php if($mobileUserAgent==false){ ?>
+<!--[if lt IE 7.]>
 <script defer type="text/javascript" src="<?php echo RELATIVE_PATH; ?>/js/pngfix.js"></script>
 <![endif]-->
 <script type="text/javascript">	
