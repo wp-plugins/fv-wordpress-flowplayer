9f4fb24ea0a61b786344d5a55b378913f1d0545e
FolioVision <FolioVision@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-21 12:59:34 +0000

    git-svn-id: https://plugins.svn.wordpress.org/fv-wordpress-flowplayer/trunk@375600 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- flowplayer.php
+++ flowplayer.php
@@ -3,7 +3,7 @@
 Plugin Name: FV Wordpress Flowplayer
 Plugin URI: http://foliovision.com/seo-tools/wordpress/plugins/fv-wordpress-flowplayer
 Description: Embed videos (FLV, H.264, and MP4) into posts or pages. Uses modified version of flowplayer (with removed FP logo and copyright notice). 
-Version: 1.2.7.1
+Version: 1.2.7
 Author: Foliovision
 Author URI: http://foliovision.com/
 */
